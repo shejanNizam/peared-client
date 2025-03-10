9fd3bd26ed7f638904691bd00d1a5dce034fbd16
nizam <nizam.shejan11@gmail.com>
2025-03-10 15:36:10 +0600

    bottom bar issue fixed

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,23 +1,9 @@
-// "use client";
+"use client";
 
-// import ProviderProfile from "@/components/profile/my-profile/ProviderProfile";
-// import UserProfile from "@/components/profile/my-profile/UserProfile";
-// import { useSelector } from "react-redux";
-
-// export default function ProfileHomePage() {
-//   const { user } = useSelector((state) => state.auth);
-
-//   return (
-//     <div className="px-4">
-//       {user?.role === "provider" ? (
-//         <>
-//           <ProviderProfile />
-//         </>
-//       ) : (
-//         <>
-//           <UserProfile />
-//         </>
-//       )}
-//     </div>
-//   );
-// }
+export default function ProfileHome() {
+  return (
+    <>
+      <h3> Profile Home </h3>
+    </>
+  );
+}
