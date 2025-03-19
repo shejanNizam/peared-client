d0bbd835e8d25d4c1701f6313e08424e67f252a8
nizam <nizam.shejan11@gmail.com>
2025-03-19 09:51:04 +0600

    add yarn setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.pearedClient',
+  appName: 'peared-client',
+  webDir: 'public'
+};
+
+export default config;
