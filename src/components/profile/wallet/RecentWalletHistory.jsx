c118b674a5ddd9f624144f29122af3b399b33334
nizam <nizam.shejan11@gmail.com>
2025-04-23 16:34:09 +0600

    Updated

--- src/components/profile/wallet/RecentWalletHistory.jsx
+++ src/components/profile/wallet/RecentWalletHistory.jsx
@@ -1,48 +1,71 @@
 import { useRecentPaymentHistoryQuery } from "@/redux/features/payment/paymentApi";
-import { List, Tag, Typography } from "antd";
+import { List, Pagination, Tag, Typography } from "antd";
 import { format } from "date-fns";
+import { useState } from "react";
 
 const { Title, Text } = Typography;
 
 export default function RecentWalletHistory() {
-  const { data } = useRecentPaymentHistoryQuery();
-  const historyData = data?.data || [];
+  const [page, setPage] = useState(1);
 
-  const formattedTransactions = historyData.map((transaction) => {
-    return {
-      label: transaction.historyName,
-      amount:
-        transaction.paymentType === "deposit"
-          ? `+ $${transaction.balance}`
-          : `- $${transaction.balance}`,
-      date: format(new Date(transaction.createdAt), "dd MMM yyyy, hh:mm a"),
-      color: transaction.paymentType === "deposit" ? "green" : "red",
-    };
-  });
+  const { data } = useRecentPaymentHistoryQuery({ page });
+  const historyData = data?.data?.paymentHistory || [];
+
+  const [currentPage, setCurrentPage] = useState(1); // State to keep track of the current page
+
+  const formattedTransactions = historyData
+    ?.slice((currentPage - 1) * 10, currentPage * 10) // Slice data based on current page
+    .map((transaction) => {
+      return {
+        label: transaction.historyName,
+        amount:
+          transaction.paymentType === "deposit"
+            ? `+ $${transaction.balance}`
+            : `- $${transaction.balance}`,
+        date: format(new Date(transaction.createdAt), "dd MMM yyyy, hh:mm a"),
+        color: transaction.paymentType === "deposit" ? "green" : "red",
+      };
+    });
 
   return (
-    <div className="bg-white rounded-xl shadow-lg p-8 w-full sm:w-96 mx-auto">
-      <Title level={4} className="text-gray-800 mb-6">
-        Recent History
-      </Title>
-      <List
-        itemLayout="horizontal"
-        dataSource={formattedTransactions}
-        renderItem={(item) => (
-          <List.Item className="hover:bg-gray-50 rounded transition">
-            <List.Item.Meta
-              title={<Text strong>{item.label}</Text>}
-              description={<Text type="secondary">{item.date}</Text>}
-            />
-            <Tag
-              color={item.color === "green" ? "green" : "volcano"}
-              style={{ fontWeight: "bold" }}
-            >
-              {item.amount}
-            </Tag>
-          </List.Item>
-        )}
-      />
-    </div>
+    <>
+      <div className="bg-white rounded-xl shadow-lg p-8 w-full sm:w-96 mx-auto">
+        <Title level={4} className="text-gray-800 mb-6">
+          Recent History
+        </Title>
+        <List
+          itemLayout="horizontal"
+          dataSource={formattedTransactions}
+          renderItem={(item) => (
+            <List.Item className="hover:bg-gray-50 rounded transition">
+              <List.Item.Meta
+                title={<Text strong>{item.label}</Text>}
+                description={<Text type="secondary">{item.date}</Text>}
+              />
+              <Tag
+                color={item.color === "green" ? "green" : "volcano"}
+                style={{ fontWeight: "bold" }}
+              >
+                {item.amount}
+              </Tag>
+            </List.Item>
+          )}
+        />
+      </div>
+
+      {/* pagination */}
+      <div className=" flex justify-center p-4">
+        <Pagination
+          defaultCurrent={1}
+          position={["bottomCenter"]}
+          showQuickJumper={true}
+          showSizeChanger={false}
+          total={data?.data?.pagination?.totalData || 0}
+          current={page}
+          onChange={(currentPage) => setPage(currentPage)}
+          pageSize={5}
+        />
+      </div>
+    </>
   );
 }
--- src/redux/features/notifications/notificationsApi.js
+++ src/redux/features/notifications/notificationsApi.js
@@ -4,10 +4,14 @@ export const notificationsApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     // notifications
     allNotifications: builder.query({
-      query: () => {
+      query: ({ page = 1, limit = 10 }) => {
         return {
           url: "/notifications",
           method: "GET",
+          params: {
+            page,
+            limit,
+          },
         };
       },
       providesTags: ["notifications"],
